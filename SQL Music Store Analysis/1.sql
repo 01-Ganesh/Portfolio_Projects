eea3c3dbe4f06afa1c7b82c793dbf915217d0d24
01-Ganesh <129285659+01-Ganesh@users.noreply.github.com>
2023-05-19 20:44:57 +0530

    Delete 1.sql

--- SQL Music Store Analysis/1.sql
+++
@@ -1,6 +0,0 @@
-/* What is the total amount each customer has spent on Zomato?*/
-
-select userid, sum(price) total_amount from product
-inner join sales on
-product.product_id = sales.product_id
-group by userid
